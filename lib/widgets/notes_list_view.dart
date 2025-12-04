767a3edf4e7ebe701502690c7b4afe626161c322
Bu4ra-3mer <boshraamer30@gmail.com>
2025-12-04 21:10:34 +0200

    add FloatingActionButton

---
+++ lib/widgets/notes_list_view.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/widgets/note_item.dart';
+
+class NotesListView extends StatelessWidget {
+  const NotesListView({super.key});
+  final data = const [];
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        return Padding(
+          padding: const EdgeInsets.symmetric(vertical: 4),
+          child: const NoteItem(),
+        );
+      },
+    );
+  }
+}
--- lib/widgets/notes_view_body.dart
+++ lib/widgets/notes_view_body.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/widgets/custom_app_bar.dart';
-import 'package:notes_app/widgets/note_item.dart';
+import 'package:notes_app/widgets/notes_list_view.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
@@ -11,7 +11,12 @@ class NotesViewBody extends StatelessWidget {
       padding: const EdgeInsets.symmetric(horizontal: 24),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.end,
-        children: [SizedBox(height: 50), CustomAppBar(), NoteItem()],
+        children: const [
+          SizedBox(height: 50),
+          CustomAppBar(),
+          SizedBox(height: 10),
+          Expanded(child: NotesListView()),
+        ],
       ),
     );
   }
