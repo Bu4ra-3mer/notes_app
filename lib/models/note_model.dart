a9c2a2349a42fe200e29fbc3c91ff36ed4a93377
Bu4ra-3mer <boshraamer30@gmail.com>
2025-12-05 16:07:14 +0200

    create note model

---
+++ lib/models/note_model.dart
@@ -0,0 +1,12 @@
+class NoteModel {
+  final String title;
+  final String subTitle;
+  final String date;
+  final int color;
+  const NoteModel({
+    required this.color,
+    required this.subTitle,
+    required this.title,
+    required this.date,
+  });
+}
