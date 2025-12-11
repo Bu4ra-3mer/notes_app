706f7ab90a80b0e974f2ae902c02308f5a622d52
Bu4ra-3mer <boshraamer30@gmail.com>
2025-12-11 20:49:57 +0200

    add add note cubit to add bottom sheet

--- lib/cubits/cubit/add_note_cubit_cubit.dart
+++ lib/cubits/cubit/add_note_cubit_cubit.dart
@@ -8,7 +8,6 @@ part 'add_note_cubit_state.dart';
 
 class AddNoteCubitCubit extends Cubit<AddNoteCubitState> {
   AddNoteCubitCubit() : super(AddNoteCubitInitial());
-
   addNote(NoteModel note) async {
     emit(AddNoteCubitLoading());
     try {
@@ -16,6 +15,7 @@ class AddNoteCubitCubit extends Cubit<AddNoteCubitState> {
       emit(AddNoteCubitSuccess());
       await notesBox.add(note);
     } catch (e) {
+    
       AddNoteCubitFailure(errorMessage: e.toString());
     }
   }
