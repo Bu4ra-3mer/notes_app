f615f273b251dddb8803fbd102f4a62899aa4032
Bu4ra-3mer <boshraamer30@gmail.com>
2025-12-11 17:02:49 +0200

    take input and validate data

--- lib/widgets/custom_text_field.dart
+++ lib/widgets/custom_text_field.dart
@@ -2,21 +2,30 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/constants.dart';
 
 class CustomTextField extends StatelessWidget {
-  const CustomTextField({
+  CustomTextField({
     super.key,
     required this.hintText,
-     this.maxLines=1,
+    this.maxLines = 1,
+    this.onSaved,
   });
   final String hintText;
   final int maxLines;
+  void Function(String?)? onSaved;
   @override
   Widget build(BuildContext context) {
-    return TextField(
+    return TextFormField(
+      onSaved: onSaved,
+      validator: (value) {
+        if (value?.isEmpty ?? true) {
+          return 'Field Is Required';
+        } else {
+          return null;
+        }
+      },
       cursorColor: kPrimaryColor,
       maxLines: maxLines,
       decoration: InputDecoration(
-       
-        hintText:hintText,
+        hintText: hintText,
         hintStyle: TextStyle(color: kPrimaryColor),
         border: builBorder(),
         enabledBorder: builBorder(),
