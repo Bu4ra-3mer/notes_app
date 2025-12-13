c5f93e15219f9371c259ba03f793f473fc5afa78
Bu4ra-3mer <boshraamer30@gmail.com>
2025-12-13 19:44:53 +0200

    edit note done

--- lib/widgets/edit_note_view_body.dart
+++ lib/widgets/edit_note_view_body.dart
@@ -1,22 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/cubits/cubit/notes_cubit_cubit.dart';
+import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/widgets/custom_app_bar.dart';
 import 'package:notes_app/widgets/custom_text_field.dart';
 
-class EditNoteViewBody extends StatelessWidget {
-  const EditNoteViewBody({super.key});
+class EditNoteViewBody extends StatefulWidget {
+  const EditNoteViewBody({super.key, required this.note});
+  final NoteModel note;
 
+  @override
+  State<EditNoteViewBody> createState() => _EditNoteViewBodyState();
+}
+
+class _EditNoteViewBodyState extends State<EditNoteViewBody> {
+  String? title, content;
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 24),
       child: Column(
         children: [
-          SizedBox(height: 50),
-          CustomAppBar(title: 'Edit Note', icon: Icons.check),
-          SizedBox(height: 50),
-          CustomTextField(hintText: 'title'),
-          SizedBox(height: 30),
-          CustomTextField(hintText: 'content', maxLines: 5),
+          const SizedBox(height: 50),
+          CustomAppBar(
+            onPressed: () {
+              widget.note.title = title ?? widget.note.title;
+              widget.note.subTitle = content ?? widget.note.subTitle;
+              widget.note.save();
+              BlocProvider.of<NotesCubitCubit>(context).featchAllNotes();
+            },
+            title: 'Edit Note',
+            icon: Icons.check,
+          ),
+          const SizedBox(height: 50),
+          CustomTextField(
+            onChanged: (p0) {
+              title = p0;
+            },
+            hintText: 'title',
+          ),
+          const SizedBox(height: 30),
+          CustomTextField(
+            onChanged: (p0) {
+              content = p0;
+            },
+            hintText: 'content',
+            maxLines: 5,
+          ),
         ],
       ),
     );
