3fd65ce5434d95d872c6ffee68da0af7240e0271
Bu4ra-3mer <boshraamer30@gmail.com>
2025-12-13 16:17:41 +0200

    display data part two

--- lib/widgets/notes_list_view.dart
+++ lib/widgets/notes_list_view.dart
@@ -22,7 +22,9 @@ class NotesListView extends StatelessWidget {
             itemBuilder: (context, index) {
               return Padding(
                 padding: const EdgeInsets.symmetric(vertical: 4),
-                child: const NoteItem(),
+                child: NoteItem(
+                  note:notes[index]
+                ),
               );
             },
           ),
